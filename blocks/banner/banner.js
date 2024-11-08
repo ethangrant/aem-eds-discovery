4507ae19111405315b2570b2534b56fdad93fb60
ethan <ethan@thepixel.com>
2024-11-08 15:30:41 +0000

    Resolve lint errors

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,4 +1,6 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
 
-export default async function(block) {
-    console.log('Banner decorate')
+export default async function (block) {
+  console.log('Banner decorate');
 }
