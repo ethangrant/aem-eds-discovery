b291b59d9f22b9a4e8ffb3cf7552b2a6193bb6ff
ethan <ethan@thepixel.com>
2024-11-08 15:32:59 +0000

    Remove content from banner.js

--- blocks/banner/banner.js
+++
@@ -1,6 +0,0 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
-
-export default async function (block) {
-  console.log('Banner decorate');
-}
