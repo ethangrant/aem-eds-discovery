1ac996e32fa44910f891aed5ab89673a9ad581dc
ethan <ethan@thepixel.com>
2024-11-29 20:39:34 +0000

    Rename to hero-heading

--- blocks/styleguide/styleguide.js
+++ blocks/styleguide/styleguide.js
@@ -3,10 +3,12 @@ import decoratePrimaryColours from './decorators/primary-colours.js';
 
 export default async function decorate(block) {
   const primaryColours = await loadFragment('/style-guide/primary-colours');
+  const titles = await loadFragment('/style-guide/titles');
 
   decoratePrimaryColours(primaryColours);
 
   block.append(primaryColours);
+  block.append(titles);
 
   // each section of the style guide would be its own fragement.
   // in this file we load each fragment then pass it through its own decorate method for styling
