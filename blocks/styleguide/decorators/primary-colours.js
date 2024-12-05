fe9902a7a10a355f55b3c131abdcdea79053b6cf
ethan <ethan@thepixel.com>
2024-12-05 20:19:29 +0000

    Re-factor

--- blocks/styleguide/decorators/primary-colours.js
+++ blocks/styleguide/decorators/primary-colours.js
@@ -1,18 +1,56 @@
 /**
  * Process each colour in the list and produce and object we can use to render the colours markup.
  * @param {HTMLUListElement} listItem
- * @returns {}
+ * @returns {Object}
  */
 function getColourObject(listItem) {
   const value = listItem.innerText;
-  let colour = value.split('-');
-  colour = colour.map((part) => part.trim());
+  const colour = value.includes('-') ? value.split('-').map((part) => part.trim()) : [];
 
   return {
     colourLabel: colour[0],
     class: `${colour[0].toLowerCase().replaceAll(' ', '-')}-bg`,
     hexCode: colour[1],
   };
+}
+
+/**
+ * @param {Object} colour
+ * @param {string} colour.colourLabel
+ * @param {string} colour.colourClass
+ * @param {string} colour.hexCode
+ * @param {boolean} showOpacity
+ * @param {Array} opacities
+ * @returns {HTMLLIElement}
+ */
+function createColourList(colour, showOpacity = false, opacities = [80, 60, 40, 20]) {
+  const liElement = document.createElement('li');
+  const colourLabel = document.createElement('p');
+  const colourBox = document.createElement('div');
+  const hexLabel = document.createElement('p');
+
+  colourLabel.innerText = colour.colourLabel;
+  hexLabel.innerText = colour.hexCode;
+  colourBox.className = `${colour.class} colour-box`;
+
+  const elements = [colourLabel, colourBox, hexLabel];
+  elements.forEach((element) => liElement.append(element));
+
+  // only show opacities for the first column.
+  if (showOpacity) {
+    const opacityContainer = document.createElement('div');
+    opacityContainer.classList = 'opacity-container';
+    opacities.forEach((opacity) => {
+      const colourBoxSmall = document.createElement('div');
+      colourBoxSmall.className = `${colour.class} colour-box-small op-${opacity}`;
+
+      opacityContainer.append(colourBoxSmall);
+    });
+
+    liElement.append(opacityContainer);
+  }
+
+  return liElement;
 }
 
 /**
@@ -28,33 +66,7 @@ function generateDom(columns) {
     ulElement.className = `col-${index + 1}`;
 
     col.forEach((colour) => {
-      const liElement = document.createElement('li');
-      const colourLabel = document.createElement('p');
-      const colourBox = document.createElement('div');
-      const hexLabel = document.createElement('p');
-      const opacities = [80, 60, 40, 20];
-
-      colourLabel.innerText = colour.colourLabel;
-      hexLabel.innerText = colour.hexCode;
-      colourBox.className = `${colour.class} colour-box`;
-
-      const elements = [colourLabel, colourBox, hexLabel];
-      elements.forEach((element) => liElement.append(element));
-
-      // only show opacities for the first column.
-      if (index === 0) {
-        const opacityContainer = document.createElement('div');
-        opacityContainer.classList = 'opacity-container';
-        opacities.forEach((opacity) => {
-          const colourBoxSmall = document.createElement('div');
-          colourBoxSmall.className = `${colour.class} colour-box-small op-${opacity}`;
-
-          opacityContainer.append(colourBoxSmall);
-        });
-
-        liElement.append(opacityContainer);
-      }
-
+      const liElement = createColourList(colour, index === 0);
       ulElement.append(liElement);
     });
 
