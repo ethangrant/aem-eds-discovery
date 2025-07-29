cf51165175c00ffd564e4979ba2a6a4687620a5a
ethan <ethan@thepixel.com>
2025-07-29 14:35:27 +0100

    basic card implementation using grid/variables

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,13 +4,15 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
+  ul.className = 'card-container grid-row';
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    li.className = 'card col-span-4 md-col-span-3';
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'card__image';
+      else div.className = 'card__body';
     });
     ul.append(li);
   });
