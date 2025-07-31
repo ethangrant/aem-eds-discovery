707b8287944bf4ca8fe707c4433a27517958abe4
ethan <ethan@thepixel.com>
2025-07-31 15:28:43 +0100

    Added DOMpurify

---
+++ blocks/raw/raw.js
@@ -0,0 +1,22 @@
+import { readBlockConfig } from '../../scripts/aem.js';
+
+// DOMPurify must be installed via npm then copied into the scripts directory.
+// This must be regularly updated https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html
+import DOMPurify from '../../scripts/purify.es.mjs';
+
+export default function decorate(block) {
+  const config = readBlockConfig(block);
+  let html = config['raw-richtext'] ?? [];
+  html = html.join('');
+  block.innerHTML = DOMPurify.sanitize(html, {
+    ALLOWED_TAGS: [
+      'b', 'i', 'em', 'strong', 'u',
+      'p', 'br', 'ul', 'ol', 'li',
+      'a', 'span', 'div', 'blockquote',
+      'code', 'pre'
+    ],
+    ALLOWED_ATTR: [
+      'href', 'title', 'target', 'rel'
+    ]
+  });
+}
